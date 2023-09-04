97a0b756693cc59bff8979af5638d13a86650484
obengben <obengbenjamin02@gmail.com>
2023-09-04 07:26:07 -0700

    malloc task5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates arguments of a function
+ * @ac: arguments count
+ * @av: arguments vector
+ * Return: pointer to concatenated string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, a = 0;
+	int count = 0;
+	char *output;
+
+	for (i = 0 ; i < ac ; i++)
+	{
+		for (j = 0 ; av[i][j] != '\0' ; j++)
+			count++;
+		count++;
+	}
+	output = malloc(sizeof(char) * count + 1);
+
+	if (output == NULL)
+		return (NULL);
+
+	for (i = 0 ; i < ac ; i++)
+	{
+		for (j = 0 ; av[i][j] != '\0' ; j++)
+		{
+			output[a] = av[i][j];
+			a++;
+		}
+		output[a++] = '\n';
+	}
+	output[a] = '\0';
+	return (output);
+}
