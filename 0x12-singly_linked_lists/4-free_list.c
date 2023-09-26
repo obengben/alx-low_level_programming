138377813d2893d04c8962772039d493502a5f0f
obengben <obengbenjamin02@gmail.com>
2023-09-25 23:42:10 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - free a linked list
+ * @head: head node
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+	       free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
